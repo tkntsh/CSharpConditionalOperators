c4d3c9ed945ff629d91d2344ac6183524f9effc6
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:17:51 +0200

    Create conditions3.cs
    Write a statement to find if 85.4 is greater than or equal to 4.
    Print to the Console.
    What is the answer?

---
+++ conditions3.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (85.4 >= 4)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
