edd640565dbdb0f781df3d849e604b2ad482135d
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:19:14 +0200

    Create conditions4.cs
    Write a statement to find if “goalKeeper” is equal to “goalie”.
    Print to the Console.
    What is the answer?

---
+++ conditions4.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if ("goalKeeper" == "goalie")
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
