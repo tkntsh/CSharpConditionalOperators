cb1fc12e012d449048477ed7c9297f0575e5fe11
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:21:13 +0200

    Create conditions8.cs
    Ask the user to “Please enter your name”.
    Produce the following message, substituting Jim for your variable:

---
+++ conditions8.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + "! How are you today?");
+        }
+    }
+}
