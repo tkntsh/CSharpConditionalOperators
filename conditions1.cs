ba2eeb9f93ef62830ee335e9a2c64c321cdc8c07
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:16:50 +0200

    Create conditions1.cs
    Write a statement to find if 10 is greater than 4.
    Print to the Console.
    What is the answer?

---
+++ conditions1.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (10 > 4)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
