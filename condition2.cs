bf81ce19d256c504525c777c42f6b85398e467ba
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:17:22 +0200

    Create condition2.cs
    Write a statement to find if -12 is equal to 12.
    Print to the Console.
    What is the answer?

---
+++ condition2.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (12 == -12)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
