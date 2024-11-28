99083ddac4120c5f74e92843b8afd88924e263c9
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:24:05 +0200

    Create conditions11.cs
    Using the code from your previous task change the variable value to 0.
    Write an else statement to produce the following message:
    
    goal-less

---
+++ conditions11.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+
+            if(score == 1)
+            {
+                Console.WriteLine("1 goal");
+            }
+            else
+            {
+                Console.WriteLine("goal-less");
+            }
+        }
+    }
+}
