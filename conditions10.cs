dd59854637771dea23750bbbdc79255d3ac33adc
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:23:31 +0200

    Create conditions10.cs
    Create a variable named score and assign it the value 1.
    Write an if statement that checks if score is equal to 1.
    Produce the following message to return a true value of 1 goal!:
    
    1 goal

---
+++ conditions10.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 1;
+
+            if(score == 1)
+            {
+                Console.WriteLine("1 goal");
+            }
+            else
+            {
+                Console.WriteLine("N/A");
+            }
+        }
+    }
+}
