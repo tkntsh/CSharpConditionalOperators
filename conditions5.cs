fcc157bad1c10f42245fbe00b25ee2522d350d5c
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:19:45 +0200

    Create conditions5.cs
    Write a statement to find if 12 is not equal to 11.
    Print to the Console.
    What is the answer?

---
+++ conditions5.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (12 != 11)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
