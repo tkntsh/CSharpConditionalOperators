05758f68af3ede976f95f22c6b8bf1921b7fe4cc
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:20:18 +0200

    Create conditions6.cs
    Create a variable named colour and assign it red.
    Write an if statement that checks if colour is equal to red.
    Produce the following to return a true value of red:
    
    Red is the colour

---
+++ conditions6.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string colour = "red";
+
+            if (colour == "red")
+            {
+                Console.WriteLine("Red is the colour");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect");
+            }
+        }
+    }
+}
